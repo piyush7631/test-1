7eeed7d7f1dea484be02577796f17364c71f82ba
Zartab Nakhwa <zartab@codewithz.com>
2022-03-04 10:18:46 +0530

    Dashboard: Data Declaration

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
+  designation: string = '';
+  username: string = '';
+  noOfTeamMembers: number = 0;
+  totalCostOfAllProjects: number = 0;
+  pendingTasks: number = 0;
+  upcomingProjects: number = 0;
+  projectCost: number = 0;
+  currentExpenditure: number = 0;
+  availableFunds: number = 0;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.designation = 'Team Leader';
+    this.username = 'Tom Smith';
+    this.noOfTeamMembers = 57;
+    this.totalCostOfAllProjects = 235;
+    this.pendingTasks = 13;
+    this.upcomingProjects = 4;
+    this.projectCost = 2341233;
+    this.currentExpenditure = 97532;
+    this.availableFunds = 59876;
   }
 
 }
